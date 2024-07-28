c4807cae2ad9422adc8f8999ac2386d385f860f9
Mac <mac@Macs-MacBook-Pro.local>
2024-07-28 11:13:40 +0100

    v3

---
+++ ecom-angular-app/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {KeycloakProfile} from "keycloak-js";
+import {KeycloakService} from "keycloak-angular";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title = 'ecom-app-angular';
+  public profile! : KeycloakProfile;
+  constructor(public keycloakService : KeycloakService) {
+  }
+
+  ngOnInit() {
+    if(this.keycloakService.isLoggedIn()){
+      this.keycloakService.loadUserProfile().then(profile=>{
+        this.profile=profile;
+      });
+    }
+  }
+
+  isLogged(){
+    return this.keycloakService.isLoggedIn()
+  }
+  async handleLogin() {
+    await this.keycloakService.login({
+      redirectUri: window.location.origin
+    });
+  }
+
+  handleLogout(){
+    this.keycloakService.logout(window.location.origin);
+  }
+}
